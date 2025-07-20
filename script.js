0882f8b5d9dafe1a0fb7d9a0efcea62a27720248
MRZAI <menuwamenuwa9@gmail.com>
2025-07-20 17:17:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const fileUploadWrapper = promptForm.querySelector(".file-upload-wrapper");
 const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 // API Setup
 const API_KEY = "AIzaSyC94xv7N1QH1lt4ebsgdxXV5PCYOXEyNYo";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent`;
 let controller, typingInterval;
 const chatHistory = [];
 const userData = { message: "", file: {} };
